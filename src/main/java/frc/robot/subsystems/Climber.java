778d53fa6ee74164b141611fcf90e6846a961e14
Yves Kloeckner <hosta.gruen@gmail.com>
2024-01-17 15:08:35 -0700

    goBottom method initial attempt

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -13,6 +14,19 @@ public class Climber extends SubsystemBase {
 
     private CANSparkMax leftClimber = new CANSparkMax(Constants.Climber.LEFT_MOTOR_ID, MotorType.kBrushless);
     private CANSparkMax rightClimber = new CANSparkMax(Constants.Climber.RIGHT_MOTOR_ID, MotorType.kBrushless);
+
+
+    // CHANGE PID NUMBERS
+    // I only need kP, kI and kD can stay at 0
+
+    private double kP = 0.0000000001;
+    private double kI = 0;
+    private double kD = 0;
+
+    // CHANGE THIS TO NUMBER OF MOTOR ROTATIONS FOR CLIMBER TO BE AT TOP
+    private double rotationsAtTop = 5;
+
+    private PIDController controller = new PIDController(kP, kI, kD);
 
     private Climber() {
         this.setName("Climber");
@@ -26,6 +40,9 @@ public class Climber extends SubsystemBase {
 
         this.leftClimber.setIdleMode(IdleMode.kBrake);
         this.rightClimber.setIdleMode(IdleMode.kBrake);
+
+        this.goBottom();
+        this.goBottom();
     }
 
     public void freeze() {
@@ -44,10 +61,27 @@ public class Climber extends SubsystemBase {
 
     public void goTop() {
         // TODO: move climber to top position
+        
     }
 
-    public void goBottom() {
-        // TODO: move climber to bottom position
+    public void goBottom() throws Exception {
+        // get current climber position
+        double leftPos = this.leftClimber.getEncoder().getPosition();
+        double rightPos = this.rightClimber.getEncoder().getPosition();
+
+        // goal position
+        double setPoint = 0.0;
+
+        if (Math.abs(leftPos-rightPos) > 0.00000001) {
+            // climbers are at different positions
+            throw new Exception("Climber arms are in different places");
+        }
+
+        this.controller.setSetpoint(setPoint);
+        // use pid to move climber to bottom position
+        while (!this.controller.atSetpoint()) {
+            this.setPower(this.controller.calculate(this.leftClimber.getEncoder().getPosition()));
+        }
     }
 
     public static Climber getInstance() {
