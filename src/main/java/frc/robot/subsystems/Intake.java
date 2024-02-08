bdd8f0e562488bd280acf8a42e6d7ba66e4d6350
SpicyPark <122577562+SpicyPark@users.noreply.github.com>
2024-02-08 16:43:39 -0700

    some changes

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -13,16 +13,15 @@ import frc.robot.subsystems.swerve.generated.TunerConstants;
 
 public class Intake extends SubsystemBase{
 
-    //private final CANSparkMax m_intakeMotor = new CANSparkMax(Constants.Intake.INTAKE_MOTOR_ID, CANSparkMax.MotorType.kBrushless);
+    private final CANSparkMax m_intakeMotor = new CANSparkMax(Constants.Intake.INTAKE_MOTOR_ID, CANSparkMax.MotorType.kBrushless);
 
     private static Intake instance = null;
-    private static boolean homing = false;
-    private int periodicControl = 0;
+    private boolean homing = false;
     private double boundingBoxOffsetX = 1.0;
     private double boundingBoxOffsetY = 1.0;
     private double limelightPoseOffset = 13.653;
     private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    private SwerveIO swerve = TunerConstants.DriveTrain;//TODO filler for no ugly red
+    private SwerveIO swerve = TunerConstants.DriveTrain;
 
     private double x, y, z, a, b, c;
 
@@ -30,32 +29,39 @@ public class Intake extends SubsystemBase{
         this.setName("Intake");
         this.register();
 
-        // this.m_intakeMotor.restoreFactoryDefaults();
-        // this.m_intakeMotor.setInverted(false);
-        // this.m_intakeMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+        this.m_intakeMotor.restoreFactoryDefaults();
+        this.m_intakeMotor.setInverted(false);
+        this.m_intakeMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
     }
 
     /**
      * Sets speed of intake motor to specified parameter
      * @param speed user specified parameter
      */
-    // public void setSpeed(double speed) {
-    //     this.m_intakeMotor.set(speed);
-    // }
+    public void setSpeed(double speed) {
+        this.m_intakeMotor.set(speed);
+    }
 
     /**
      * Sets motor speed at a certain speed
-    //  */
-    // public void startIntake() {
-    //     this.setSpeed(0.1);//test this when possible. will likely be set high to something like 0.97 or 1
-    // }
+     */
+    public void startIntake() {
+        this.setSpeed(0.7);
+    }
+
+    /**
+     * Spins intake backwards in the case of a note being caught
+     */
+    public void reverseIntake() {
+        this.setSpeed(-0.2);
+    }
     
-    // /**
-    //  * Stops the motor
-    //  */
-    // public void stopIntake() {
-    //     this.setSpeed(0);
-    // }
+    /**
+     * Stops the motor
+     */
+    public void stopIntake() {
+        this.setSpeed(0);
+    }
 
     /**
      * Detects whether or not a note is within the Limelight's POV
@@ -86,7 +92,7 @@ public class Intake extends SubsystemBase{
     }
 
     public double calculateX(double boundingBoxWidth) {
-        return 600 / boundingBoxWidth; //TODO implement this method
+        return 600 / boundingBoxWidth;
     }
 
     public double calculateZ(double x, double y, double a) {
@@ -106,11 +112,11 @@ public class Intake extends SubsystemBase{
     }
 
     public void setHoming(boolean b) {
-        Intake.homing = b;
+        this.homing = b;
     }
 
     public boolean getHoming() {
-        return Intake.homing;
+        return this.homing;
     }
 
     public void periodic() {
@@ -121,20 +127,22 @@ public class Intake extends SubsystemBase{
         this.z = this.calculateZ(x, y, b);
         this.a = this.calculateHeading(x, y, z);
         
-        if (homing) {
-            if (this.noteDetected()) {
-                //this.startIntake();
-                this.swerve.setTargetHeading(this.a);
-                if (Math.abs(this.getNoteDegreeX()) < 10 && this.x < 24) this.swerve.setDriveState(DriveState.ROBOT_CENTRIC);
-                else this.swerve.setDriveState(DriveState.FIELD_CENTRIC);
-            }
+        if (homing && this.noteDetected()) {
+            this.startIntake();
+            this.swerve.setTargetHeading(this.a);
+            if (Math.abs(this.getNoteDegreeX()) < 10 && this.x < 24) this.swerve.setDriveState(DriveState.ROBOT_CENTRIC);
+            else this.swerve.setDriveState(DriveState.FIELD_CENTRIC);
         }
         else {
-            //this.stopIntake();
+            this.stopIntake();
             this.swerve.setDriveState(DriveState.FIELD_CENTRIC);
         }
     }
 
+    /**
+     * Singleton architecture which returns the singular instance of Intake
+     * @return the instance (which is instantiated when first called)
+     */
     public static Intake getInstance(){
         if (instance == null) instance = new Intake();
         return instance;
--- src/main/java/frc/robot/subsystems/LED.java
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -1,228 +1,234 @@
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import edu.wpi.first.wpilibj.AddressableLED;
-// import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
-// public class LED extends SubsystemBase{
+public class LED extends SubsystemBase{
 
-//     private static LED instance = null;
+    private static LED instance = null;
 
-//     private AddressableLED control = new AddressableLED(Constants.LED.LED_CHANNEL_ID);
-//     private AddressableLEDBuffer buffer = new AddressableLEDBuffer(Constants.LED.NUM_LEDS);
+    private AddressableLED control = new AddressableLED(Constants.LED.LED_CHANNEL_ID);
+    private AddressableLEDBuffer buffer = new AddressableLEDBuffer(Constants.LED.NUM_LEDS);
 
-//     private boolean initialAlliance = false;//TODO implement this
-//     private boolean restingState = false;
-//     private boolean noteDetectedState = false;
-//     private boolean homingNoteState = false;
-//     private boolean hasNoteState = false;
-//     private boolean homingAprilTagState = false;
-//     private boolean policeMode = false;
+    private boolean initialAlliance = false;//TODO implement this
 
-//     private int allianceColorCutoff = buffer.getLength() / 3;
+    private enum State {
+        RESTING,
+        NOTE_DETECTED,
+        HOMING_NOTE,
+        HAS_NOTE,
+        HOMING_APRILTAG
+    }
 
-//     /**
-//      * Constructor for LED which registers the subsystem and sets a specified portion of the LED lights to the alliance color
-//      */
-//     private LED() {
-//         this.setName("LED");
-//         this.register();
+    State RobotState = State.RESTING;
 
-//         control.setLength(buffer.getLength());
-//         if (initialAlliance) {
-//             for (int i = 0; i < allianceColorCutoff; i++) {
-//                 buffer.setRGB(i, 255, 0, 0);
-//             }
-//         }
-//         else {
-//             for (int i = 0; i < allianceColorCutoff; i++) {
-//                 buffer.setRGB(i, 0, 0, 255);
-//             }
-//         }
-//         control.setData(buffer);
-//     }
+    private int policeMode = 0;
+    private boolean policeModeEnabled = false;
 
-//     /**
-//      * Sets restingState to specified parameter whilst setting all other state booleans to false
-//      * @param b user specified boolean which restingState is set to
-//      */
-//     public void setRestingState(boolean b) {
-//         this.restingState = b;
+    private int policeModeControl1 = 0;
+    private int policeModeControl2 = 0;
+    private int policeModeControl3 = 0;
+    private int policeModeColorControl3 = 0;
 
-//         this.noteDetectedState = !b;
-//         this.homingNoteState = !b;
-//         this.hasNoteState = !b;
-//         this.homingAprilTagState = !b;
-//     }
+    private int allianceColorCutoff = buffer.getLength() / 3;
 
-//     /**
-//      * Sets noteDetectedState to specified parameter whilst setting all other state booleans to false
-//      * @param b user specified boolean which noteDetectedState is set to
-//      */
-//     public void setNoteDetectedState(boolean b) {
-//         this.noteDetectedState = b;
+    /**
+     * Constructor for LED which registers the subsystem and sets a specified portion of the LED lights to the alliance color
+     */
+    private LED() {
+        this.setName("LED");
+        this.register();
 
-//         this.restingState = !b;
-//         this.homingNoteState = !b;
-//         this.hasNoteState = !b;
-//         this.homingAprilTagState = !b;
-//     }
+        control.setLength(buffer.getLength());
+        if (initialAlliance) {//true is red
+            for (int i = 0; i < allianceColorCutoff; i++) {
+                buffer.setRGB(i, 255, 0, 0);
+            }
+        }
+        else {//false is blue
+            for (int i = 0; i < allianceColorCutoff; i++) {
+                buffer.setRGB(i, 0, 0, 255);
+            }
+        }
+        control.setData(buffer);
+    }
 
-//     /**
-//      * Sets homingNoteState to specified parameter whilst setting all other state booleans to false
-//      * @param b user specified boolean which homingNoteState is set to
-//      */
-//     public void setHomingNoteState(boolean b) {
-//         this.homingNoteState = b;
+    public void setState(State s) {
+        switch(s) {
+            case RESTING: RobotState = State.RESTING;
+            case NOTE_DETECTED: RobotState = State.NOTE_DETECTED;
+            case HOMING_NOTE: RobotState = State.HOMING_NOTE;
+            case HAS_NOTE: RobotState = State.HAS_NOTE;
+            case HOMING_APRILTAG: RobotState = State.HOMING_APRILTAG;
+        }
+    }
 
-//         this.restingState = !b;
-//         this.noteDetectedState = !b;
-//         this.hasNoteState = !b;
-//         this.homingAprilTagState = !b;
-//     }
+    public void togglePoliceMode(boolean b) {
+        this.policeModeEnabled = b;
+    }
 
-//     /**
-//      * Sets hasNoteState to specified parameter whilst setting all other state booleans to false
-//      * @param b user specified boolean which hasNoteState is set to
-//      */
-//     public void setHasNoteState(boolean b) {
-//         this.hasNoteState = b;
+    public void setPoliceMode(int mode) {
+        this.policeMode = mode;
+    }
 
-//         this.restingState = !b;
-//         this.noteDetectedState = !b;
-//         this.homingNoteState = !b;
-//         this.homingAprilTagState = !b;
-//     }
+    public void periodic() {
+        switch(RobotState) {
+            case RESTING:
+                for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
+                    buffer.setRGB(i, 255, 255, 255);//white
+                }
+                control.setData(buffer);
+                break;
+            case NOTE_DETECTED:
+                for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
+                    buffer.setRGB(i, 115, 81, 226);//cube purple
+                }
+                control.setData(buffer);
+                break;
+            case HOMING_NOTE:
+                for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
+                    buffer.setRGB(i, 255, 183, 3);//cone yellow
+                }
+                control.setData(buffer);
+                break;
+            case HAS_NOTE:
+                for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
+                    buffer.setRGB(i, 252, 85, 36);//note orange
+                }
+                control.setData(buffer);
+                break;
+            case HOMING_APRILTAG:
+                for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
+                    buffer.setRGB(i, 30, 225, 30);//limelight green
+                }
+                control.setData(buffer);
+                break;
+        }
 
-//     /**
-//      * Sets homingAprilTagState to specified parameter whilst setting all other state booleans to false
-//      * @param b user specified boolean which homingAprilTagState is set to
-//      */
-//     public void setHomingAprilTagState(boolean b) {
-//         this.homingAprilTagState = b;
+        if (policeModeEnabled) {
+            if (policeMode == 1) {//solid alternating color
+                policeModeControl1++;
+                if (policeModeControl1 % 2 == 0) {
+                    for (int i = 0; i < buffer.getLength(); i++) {
+                        buffer.setRGB(i, 255, 0, 0);
+                    }
+                    control.setData(buffer);
+                }
+                else {
+                    for (int i = 0; i < buffer.getLength(); i++) {
+                        buffer.setRGB(i, 0, 0, 255);
+                    }
+                    control.setData(buffer);
+                }
+            }
+            else if (policeMode == 2) {//one flash each color on two halves
+                policeModeControl2++;
+                if (policeModeControl2 % 2 == 0) {
+                    for (int i = 0; i < buffer.getLength() / 2; i++) {
+                        buffer.setRGB(i, 255, 0, 0);
+                    }
+                    for (int i = buffer.getLength() / 2; i < buffer.getLength(); i++) {
+                        buffer.setRGB(i, 0, 0, 0);
+                    }
+                    control.setData(buffer);
+                }
+                else {
+                    for (int i = 0; i < buffer.getLength() / 2; i++) {
+                        buffer.setRGB(i, 0, 0, 0);
+                    }
+                    for (int i = buffer.getLength() / 2; i < buffer.getLength(); i++) {
+                        buffer.setRGB(i, 0, 0, 255);
+                    }
+                    control.setData(buffer);
+                }
+            }
+            else if (policeMode == 3) {//three flashes each color on two halves
+                policeModeControl3++;
+                if (policeModeColorControl3 % 2 == 0) {
+                    if (policeModeControl3 % 8 == 0) {
+                        for (int i = 0; i < buffer.getLength() / 2; i++) {
+                            buffer.setRGB(i, 0, 0, 0);
+                        }
+                        control.setData(buffer);
+                    }
 
-//         this.restingState = !b;
-//         this.noteDetectedState = !b;
-//         this.homingNoteState = !b;
-//         this.hasNoteState = !b;
-//     }
-
-//     public void periodic() {
-//         if (restingState) {
-//             for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
-//                 buffer.setRGB(i, 255, 255, 255);//white
-//             }
-//             control.setData(buffer);
-//         }
-//         else if (noteDetectedState) {
-//             for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
-//                 buffer.setRGB(i, 115, 81, 226);//cube purple
-//             }
-//             control.setData(buffer);
-//         }
-//         else if (homingNoteState) {
-//             for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
-//                 buffer.setRGB(i, 255, 183, 3);//cone yellow
-//             }
-//             control.setData(buffer);
-//         }
-//         else if (hasNoteState) {
-//             for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
-//                 buffer.setRGB(i, 252, 85, 36);//note orange
-//             }
-//             control.setData(buffer);
-//         }
-//         else if (homingAprilTagState) {
-//             for (int i = allianceColorCutoff; i < buffer.getLength(); i++) {
-//                 buffer.setRGB(i, 30, 225, 30);//limelight green
-//             }
-//             control.setData(buffer);
-//         }
-
-//         else if (policeMode) {
-//             int policeModeControl = 0;
-//             int policeModeColorControl = 0;
-//             policeModeControl++;
-//             if (policeModeColorControl % 2 == 0) {
-//                 if (policeModeControl % 8 == 0) {
-//                     for (int i = 0; i < buffer.getLength() / 2; i++) {
-//                         buffer.setRGB(i, 0, 0, 0);
-//                     }
-//                     control.setData(buffer);
-//                 }
-
-//                 else if (policeModeControl % 4 == 0) {
-//                     for (int i = 0; i < buffer.getLength() / 2; i++) {
-//                         buffer.setRGB(i, 0, 0, 255);
-//                     }
-//                     control.setData(buffer);
-
-
-//                     if (policeModeControl == 20) {
-//                         policeModeControl = 0;
-//                         policeModeColorControl++;
-//                     }
-//                 }
-
-//                 for (int i = buffer.getLength() / 2; i < buffer.getLength(); i++) {
-//                     buffer.setRGB(i, 0, 0, 0);
-//                 }
-//                 control.setData(buffer);
-//             }
-//             else {
-//                 if (policeModeControl % 8 == 0) {
-//                     for (int i = buffer.getLength() / 2; i < buffer.getLength(); i++) {
-//                         buffer.setRGB(i, 0, 0, 0);
-//                     }
-//                     control.setData(buffer);
-//                 }
-
-//                 else if (policeModeControl % 4 == 0) {
-//                     for (int i = buffer.getLength() / 2; i < buffer.getLength(); i++) {
-//                         buffer.setRGB(i, 255, 0, 0);
-//                     }
-//                     control.setData(buffer);
+                    else if (policeModeControl3 % 4 == 0) {
+                        for (int i = 0; i < buffer.getLength() / 2; i++) {
+                            buffer.setRGB(i, 0, 0, 255);
+                        }
+                        control.setData(buffer);
 
 
-//                     if (policeModeControl == 20) {
-//                         policeModeControl = 0;
-//                         policeModeColorControl++;
-//                     }
-//                 }
+                        if (policeModeControl3 == 20) {
+                            policeModeControl3 = 0;
+                            policeModeColorControl3++;
+                        }
+                    }
 
-//                 for (int i = 0; i < buffer.getLength() / 2; i++) {
-//                     buffer.setRGB(i, 0, 0, 0);
-//                 }
-//                 control.setData(buffer);
-//             }
-//         }
+                    for (int i = buffer.getLength() / 2; i < buffer.getLength(); i++) {
+                        buffer.setRGB(i, 0, 0, 0);
+                    }
+                    control.setData(buffer);
+                }
+                else {
+                    if (policeModeControl3 % 8 == 0) {
+                        for (int i = buffer.getLength() / 2; i < buffer.getLength(); i++) {
+                            buffer.setRGB(i, 0, 0, 0);
+                        }
+                        control.setData(buffer);
+                    }
 
-//         if (Intake.getInstance().noteDetected() && Intake.getInstance().getHoming()) {
-//             this.setHomingNoteState(true);
-//         }
-//         else if (Intake.getInstance().noteDetected()) {
-//             this.setNoteDetectedState(true);
-//         }
-//         else if (!Intake.getInstance().noteDetected() && !Index.getInstance().hasNote()) {
-//             this.setRestingState(true);
-//         }
-//         else if (Index.getInstance().hasNote()) {
-//             this.setHasNoteState(true);
-//         }
-//         else if (Shooter.getInstance().getHoming()) {
-//             this.setHomingAprilTagState(true);
-//         }
+                    else if (policeModeControl3 % 4 == 0) {
+                        for (int i = buffer.getLength() / 2; i < buffer.getLength(); i++) {
+                            buffer.setRGB(i, 255, 0, 0);
+                        }
+                        control.setData(buffer);
+
+
+                        if (policeModeControl3 == 20) {
+                            policeModeControl3 = 0;
+                            policeModeColorControl3++;
+                        }
+                    }
+
+                    for (int i = 0; i < buffer.getLength() / 2; i++) {
+                        buffer.setRGB(i, 0, 0, 0);
+                    }
+                    control.setData(buffer);
+                }
+            }
+            else if (policeMode == 4) {//segmented flashes
+
+            }
+        }
+
+        if (Intake.getInstance().noteDetected() && Intake.getInstance().getHoming()) {
+            this.setState(State.HOMING_NOTE);
+        }
+        else if (Intake.getInstance().noteDetected()) {
+            this.setState(State.NOTE_DETECTED);
+        }
+        else if (!Intake.getInstance().noteDetected() && !Index.getInstance().hasNote()) {
+            this.setState(State.RESTING);
+        }
+        else if (Index.getInstance().hasNote()) {
+            this.setState(State.HAS_NOTE);
+        }
+        else if (Shooter.getInstance().getHoming()) {
+            this.setState(State.HOMING_APRILTAG);
+        }
     
-//         //123, 231, 244 giorgio blue :)
-//     }
+        //123, 231, 244 giorgio blue :)
+    }
 
-//     /**
-//      * Singleton architecture which returns the singular instance of LED
-//      * @return the instance (which is instantiated when first called)
-//      */
-//     public static LED getInstance(){
-//         if (instance == null) instance = new LED();
-//         return instance;
-//     }
-// }
+    /**
+     * Singleton architecture which returns the singular instance of LED
+     * @return the instance (which is instantiated when first called)
+     */
+    public static LED getInstance(){
+        if (instance == null) instance = new LED();
+        return instance;
+    }
+}
