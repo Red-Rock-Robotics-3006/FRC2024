faae923bee696230d1b03bb670b17da1b880ed10
Red-Rock-Robotics-3006 <54916973+Red-Rock-Robotics-3006@users.noreply.github.com>
2024-04-02 14:16:50 -0600

    no police haha

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,7 @@ public final class Constants {
   public static class Settings {
     public static final boolean INTAKE_HOMING_ENABLED = false;
     public static final boolean SHOOTER_HOMING_ENABLED = true;
-    public static final boolean POLICE_MODE_ENABLED = true;
+    public static final boolean POLICE_MODE_ENABLED = false;
     public static final boolean ABSOLUTE_LOCALIZATION = false;
   }
 }
